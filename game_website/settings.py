faf7cbfc64e86772a48bfea039a543e228b618e1
12calert <12calert@wp.pl>
2022-10-31 05:37:55 +0000

    More fire

--- game_website/settings.py
+++ game_website/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'chat',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'chat'
 ]
 
 MIDDLEWARE = [
