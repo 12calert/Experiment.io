b1483a5006744e0158085a43982cca0685a01fac
alberto <albertovitri@outlook.com>
2023-01-29 18:49:50 +0000

    changes for hosting on render

--- game_website/settings.py
+++ game_website/settings.py
@@ -26,10 +26,12 @@ SECRET_KEY = '1u6r1oki8pk9=g)&x%_7glcn-ja^@@o&ggordj0$krek-5y0ye'
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
