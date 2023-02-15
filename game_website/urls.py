191eb85d0082dd75d40a27395bfbb4d985b08dcf
alberto <albertovitri@outlook.com>
2023-02-15 18:16:16 +0000

    chat changes and clean up

--- game_website/urls.py
+++ game_website/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include #, re_path
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
@@ -32,6 +32,11 @@ urlpatterns = [
     path('home', views.homepage, name='home'),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
     path('researcher_login/', views.researcher_login, name='researcher_login'),
+    path('all_rooms/', views.all_rooms, name="all_rooms"),
+
+    path('all_rooms/actionUrl', views.create_room),
     path('game_view/<str:room_name>/', views.game_view, name='game_view'),
+    # tried with regex but need help
+    #re_path(r'^all_rooms/game_view/(?P<room_name>)/$', views.game_view, name='game_view')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
