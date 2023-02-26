c49d3c0fc84e51a67dbda7ca8a3134f350ffea8b
Alberto Rodriguez <u14ar20@abdn.ac.uk>
2023-02-26 20:07:55 +0000

    role allocation

--- game_website/routing.py
+++ game_website/routing.py
@@ -1,8 +1,8 @@
 # chat/routing.py
-from django.urls import re_path
+from django.urls import re_path, path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/game_view/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/all_rooms/game_view/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
