333578e24ae4ae87c5f32ce002edef26b83d2f57
Athinoulla Konstantinou <u16ak20@abdn.ac.uk>
2023-03-10 22:23:36 +0000

    Re-created the researcher registration testing

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,101 +4,66 @@ from .models import Researcher
 from game_website.forms import ResearcherRegisterForm
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Researcher
- 
-class ResearcherRegistrationTestCase(TestCase):
-    def test_valid_registration(self):
-        # Create a user for authentication purposes
-        user = User.objects.create_user(
-            username='testuser',
-            password='testpass'
-        )
 
-        # Define test data
-        form_data = {
+# Testing of Researcher registration
+class ResearcherRegistrationTests(TestCase):
+    # Test that the registration page loads successfully:
+    def test_registration_page_loads_successfully(self):
+        url = reverse('researcher_registration')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def setUp(self):
+        self.valid_data = {
             'forename': 'John',
             'surname': 'Doe',
             'username': 'johndoe',
             'email': 'johndoe@example.com',
-            'password': 'password'
+            'password': 'password123'
         }
-
-        # Instantiate the form with test data
-        form = ResearcherRegisterForm(data=form_data)
-
-        # Verify that the form is valid
-        self.assertTrue(form.is_valid())
-
-        # Save the form data to the database
-        researcher = Researcher(
-            user=user,
-            forename=form.cleaned_data['forename'],
-            surname=form.cleaned_data['surname'],
-            email=form.cleaned_data['email'],
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password']
-        )
-        researcher.save()
-
-        # Verify that the researcher was added to the database
-        self.assertEqual(Researcher.objects.count(), 1)
-
-    def test_invalid_registration(self):
-        # Define test data with missing required fields
-        form_data = {
+        
+        self.invalid_data = {
             'forename': 'John',
             'surname': 'Doe',
-            'username': 'johndoe'
+            'username': 'johndoe',
+            'email': 'invalid_email',
+            'password': 'password123'
         }
-
-        # Instantiate the form with test data
-        form = ResearcherRegisterForm(data=form_data)
-
-        # Verify that the form is invalid
-        self.assertFalse(form.is_valid())
     
-    def test_form_fields(self):
-        # Check if all form fields are present
-        form = ResearcherRegisterForm()
-        self.assertTrue('forename' in form.fields)
-        self.assertTrue('surname' in form.fields)
-        self.assertTrue('username' in form.fields)
-        self.assertTrue('email' in form.fields)
-        self.assertTrue('password' in form.fields)
-
-class ResearcherLoginTestCase(TestCase):
-    def setUp(self):
-        # Set up a test user and researcher instance
-        self.username = 'testuser'
-        self.password = 'testpass'
-        self.user = User.objects.create_user(
-            username=self.username, 
-            password=self.password
-        )
-        self.researcher = Researcher.objects.create(
-            user=self.user,
-            forename='Test',
-            surname='User',
-            email='testuser@example.com',
-            username=self.username,
-            password=self.password,
-            approved=True
-        )
-
-    def test_researcher_login_view_success(self):
-        # Test that a researcher can log in successfully
-        response = self.client.post(reverse('researcher_login'), {
-            'username': self.username,
-            'password': self.password,
-            })
-        # The expected response for a successful login is a redirect
-        self.assertEqual(response.status_code, 302)
-
-    def test_researcher_login_view_failure(self):
-        # Test that a researcher cannot log in with incorrect password
-        response = self.client.post(reverse('researcher_login'), {
-            'username': self.username,
-            'password': 'wrongpassword',
-            })
-        # The expected response for a failed login is a 200 status code  
+    def test_register_valid_data(self):
+        # Make a POST request to the researcher_registration view with valid form data
+        response = self.client.post(reverse('researcher_registration'), self.valid_data)
+        
+        # Check that the response is a redirect to the home page
+        self.assertRedirects(response, reverse('home'))
+        
+        # Check that a new User object was created with the correct data
+        self.assertTrue(User.objects.filter(username=self.valid_data['username']).exists())
+        user = User.objects.get(username=self.valid_data['username'])
+        self.assertEqual(user.email, self.valid_data['email'])
+        self.assertEqual(user.first_name, self.valid_data['forename'])
+        self.assertEqual(user.last_name, self.valid_data['surname'])     
+        self.assertFalse(user.is_active) # The user should not be active yet
+ 
+        # Check that a new Researcher object was created with the correct userkey
+        self.assertTrue(Researcher.objects.filter(userkey=user).exists())
+        researcher = Researcher.objects.get(userkey=user)
+        self.assertIsNotNone(researcher.researcher_id)
+        
+        # Check that the user is not active until they are authenticated
+        self.assertFalse(user.is_active)
+        
+    def test_register_invalid_data(self):
+        # Make a POST request to the researcher_registration view with invalid form data
+        response = self.client.post(reverse('researcher_registration'), self.invalid_data)
+        
+        # Check that the response is not a redirect
         self.assertEqual(response.status_code, 200)
+        
+        # Check that the form is invalid
+        self.assertFalse(ResearcherRegisterForm(data=self.invalid_data).is_valid())
+        
+        # Check that no new User or Researcher objects were created
+        self.assertFalse(User.objects.filter(username=self.invalid_data['username']).exists())
+        self.assertFalse(Researcher.objects.filter(userkey__username=self.invalid_data['username']).exists())
+
