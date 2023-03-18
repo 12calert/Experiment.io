7fd2253158d09e181c30d14346b7446ad1e6499a
Athinoulla Konstantinou <u16ak20@abdn.ac.uk>
2023-03-18 16:54:10 +0000

    password reset option

--- game_website/urls.py
+++ game_website/urls.py
@@ -30,8 +30,14 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import views as auth_views
  
-
+from django.contrib.auth import views as auth_views
+ 
 urlpatterns = [
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
     path('admin/', admin.site.urls, name="admin"),
     path('accounts/', include('accounts.urls'), name='accounts'),
     path('', views.homepage, name='home'),
