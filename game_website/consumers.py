dd5e0ab385f20d2f399f6ad3c9f07540d8b46b0a
Athinoulla Konstantinou <u16ak20@abdn.ac.uk>
2023-04-03 21:46:06 +0100

    commnets on consumers.ppy

--- game_website/consumers.py
+++ game_website/consumers.py
@@ -1,12 +1,11 @@
 # adapted from https://channels.readthedocs.io/en/stable/tutorial/part_3.html
 import json
-
 from asgiref.sync import sync_to_async
 from accounts.models import Game
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-# this whole file needs refactoring
 class ChatConsumer(AsyncWebsocketConsumer):
+    """ This function connects a user to a websocket chat. """
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
@@ -19,16 +18,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(self.room_group_name, {"type": "announcement", "message": "A user has connected!" , "finished":False})
         await self.accept()
         return
-
-        
-
+    
+    """ This function disconnects a user from a websocket chat."""
     async def disconnect(self, close_code):
         # Leave room group
         self.room.users -= 1
         await sync_to_async(self.room.save)()
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # Receive message from WebSocket
+    """ Receive message from WebSocket. """
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
@@ -46,7 +44,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Send message to room group
         await self.channel_layer.group_send(self.room_group_name, {"type": "chat_message", "message": message, "role": role, "finished":finished})
 
-    # Receive message from room group
+    """ Receive message from room group. """
     async def chat_message(self, event):
         message = event["message"]
         role = event["role"]
@@ -60,12 +58,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             # Send message to WebSocket
             await self.send(text_data=json.dumps({"type": "chat_message", "message": message, "role": role, "finished":finished}))
     
+    """ This function sends an announcement message to the connected websocket clients. """
     async def announcement(self,event):
         message = event["message"]
         finished = event["finished"]
 
         await self.send(text_data=json.dumps({"type":"announcement", "message":message, "finished":finished}))
-    
+        
+    """ This function sends a move message to the connected websocket clients."""
     async def move( self, event ):
         message = event[ "message" ]
 
