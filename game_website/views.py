5a8e81d4bb82c9102eb8c7b6c5c20e47fafe7f4e
alberto <albertovitri@outlook.com>
2023-04-12 10:31:02 +0100

    adding dummy games

--- game_website/views.py
+++ game_website/views.py
@@ -14,9 +14,7 @@ from django.urls import reverse
 # custom shapes module allows us to easily change values (also to apply custom conditions)
 from game_website.shapes import randomColour, randomShape
 import game_website.serialize as customSerializers
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-from rest_framework.response import Response
+
 # helper methods
 """checks a request to see if it is an ajax request"""
 def is_ajax(request):
@@ -36,11 +34,11 @@ def homepage(request):
         if chooseGame.is_valid():   # if the form is valid (no inputs are invalid)
             #send to appropriate game rooms page
             game_type = chooseGame.cleaned_data.get("game_choice")
-            if game_type == "MG":
+            if game_type == "MT":
                 return redirect("map_task", game = game_type)
             # this where the other game type redirects would go
             else:
-                return render(request, 'map_task.html', context)
+                return render(request, 'home.html', context)
             
     return render(request, 'home.html', context)
 
